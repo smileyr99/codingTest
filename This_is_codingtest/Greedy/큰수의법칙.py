9da5b3033754b700b77907381cabef43c66b4ced
smileyr909 <smileyr909@gmail.com>
2022-07-31 22:14:31 +0900

    greedy commit

--- This_is_codingtest/Greedy/큰수의법칙.py
+++ This_is_codingtest/Greedy/큰수의법칙.py
@@ -6,8 +6,8 @@ nums = list(map(int, sys.stdin.readline().split()))
 nums.sort()
 nums.reverse()
 
-mul = M // K
-re = M % K
-result = nums[0] * mul * K + nums[1] * re
+mul = M // K    # 큰 수가 더해지는 횟수
+re = M % K      # 다음 큰수가 더해지는 횟수
+result = nums[0] * mul * K + nums[1] * re   # 결과 계산
 
 print(result)
