1a52fe806a06bfdbfb2f97d27be21a2f5c2e9fc2
smileyr909 <smileyr909@gmail.com>
2022-08-06 14:09:52 +0900

    DFS/BFS commit

---
+++ This_is_codingtest/DFS_BFS/BFS.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [0] * len(graph)
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = 1
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if visited[i] == 0:
+                visited[i] = 1
+                queue.append(i)
+
+
+bfs(graph, 1, visited)
