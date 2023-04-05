99149f1cfe5c13e241a8a00516015cd4c5816734
yerim <87894946+smileyr99@users.noreply.github.com>
2023-04-06 01:23:22 +0900

    [Silver II] Title: 222-풀링, Time: 468 ms, Memory: 75112 KB -BaekjoonHub

--- 백준/Silver/17829. 222－풀링/222－풀링.py
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -1,19 +1,21 @@
-n=int(input())
-nums=[]
-for i in range(n):
-    tmp=list(map(int, input().split()))
-    nums.append(tmp)
-def pooling(size, x, y):
-    mid=size//2
-    if size==2:
-        answer=[nums[x][y], nums[x+1][y], nums[x][y+1], nums[x+1][y+1]]
-        answer.sort()
-        return answer[-2]
-    lt=pooling(mid, x, y)
-    rt=pooling(mid, x+mid, y)
-    lb=pooling(mid, x, y+mid)
-    rb=pooling(mid, x+mid, y+mid)
-    answer=[lt, rt, lb, rb]
-    answer.sort()
-    return answer[-2]
-print(pooling(n, 0, 0))
+
+import sys
+
+
+def solution():
+    n = int(sys.stdin.readline())
+    board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+    while len(board[-1]) != 1:
+        new = []
+        for i in range(0, len(board), 2):
+            tmp = []
+            for j in range(0, len(board), 2):
+                calc = sorted([board[i][j], board[i + 1][j], board[i][j + 1], board[i + 1][j + 1]], reverse=True)
+                tmp.append(calc[1])
+            new.append(tmp)
+        board = new
+    print(board[0][0])
+
+
+solution()
