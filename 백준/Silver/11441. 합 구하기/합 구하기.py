4d26677ab18d1a7f9bee18abacd880d67c05b699
yerim <87894946+smileyr99@users.noreply.github.com>
2023-04-08 23:40:37 +0900

    [Silver III] Title: 합 구하기, Time: 224 ms, Memory: 42340 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+A = []
+A =list(map(int, input().split()))
+
+ans = [0]*(N+1)
+
+for k in range(N):
+    ans[k] = ans[k-1]+A[k]
+
+M = int(input())
+
+for k in range(M):
+    i, j = map(int, input().split())
+
+    res = ans[j-1]-ans[i-2] 
+    sys.stdout.write(str(res)+'\n')
